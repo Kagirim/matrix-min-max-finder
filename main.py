909e183f1fa721fb44c68bccae1e5855fe51334b
kagirim <morriskagiri@gmail.com>
2023-02-17 00:58:01 +0300

    Added two more fail tests

--- main.py
+++ main.py
@@ -24,3 +24,13 @@ assert max_row_min_col(matrix) == {17}
 # Test case 2
 matrix = [[100, 60, 20, 50], [70, 80, 10, 90], [10, 50, 44, 30]]
 assert max_row_min_col(matrix) == {50}
+
+# Test case 3
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = max_row_min_col(matrix)
+assert result == {3, 6}, f"Failed: {result}"
+
+# Test case 4
+matrix = [[5, 16, 20], [9, 11, 18], [15, 16, 20]]
+result = max_row_min_col(matrix)
+assert result == {16}, f"Failed: {result}"
--- tests.py
+++ tests.py
@@ -9,3 +9,13 @@ print(f"{matrix} : {result}")  # {17}
 matrix = [[100, 60, 20, 50], [70, 80, 10, 90], [10, 50, 44, 30]]
 result = max_row_min_col(matrix)
 print(f"{matrix} : {result}")   # {50}
+
+# Test case 3
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = max_row_min_col(matrix)
+print(f"{matrix} : {result}")
+
+# Test case 4
+matrix = [[5, 16, 20], [9, 11, 18], [15, 16, 20]]
+result = max_row_min_col(matrix)
+print(f"{matrix} : {result}")
