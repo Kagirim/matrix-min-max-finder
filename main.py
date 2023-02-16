fc9fd441e211da6047d1be3dcb3ebaafc20d2e9c
kagirim <morriskagiri@gmail.com>
2023-02-17 00:34:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def max_in_row_min_in_col(matrix):
+    # Create empty sets to store the maximum in row and minimum in column, respectively
+    max_in_row = set()
+    min_in_col = set()
+
+    # For each row in the matrix, find the maximum element in that row and add it to max_in_row set
+    for row in matrix:
+        max_in_row.add(max(row))
+
+    # For each column in the matrix, find the minimum element in that column and add it to min_in_col set
+    for col in range(len(matrix[0])):
+        # To find the minimum element in a column, we can iterate through all rows and take the minimum value
+        # of the element in that column using a generator expression
+        min_in_col.add(min(row[col] for row in matrix))
+
+    # Return the intersection of the two sets, which contains only the elements that are in both sets
+    return max_in_row.intersection(min_in_col)
+
+# Tests
+# Test case 1
+matrix = [[5, 16, 20], [9, 11, 18], [15, 16, 17]]
+assert max_in_row_min_in_col(matrix) == {17}
+
+# Test case 2
+matrix = [[100, 60, 20, 50], [70, 80, 10, 90], [10, 50, 44, 30]]
+assert max_in_row_min_in_col(matrix) == {50}
