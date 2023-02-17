174e09b03fc1c5341f476c4ab47532a1e908a6ea
Morris Kagiri <48337199+Kagirim@users.noreply.github.com>
2023-02-17 07:59:59 +0300

    Update main.py
    added comments on time complexity

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 def max_row_min_col(matrix):
     # Create empty sets to store the maximum in row and minimum in column, respectively
-    max_in_row = set()
-    min_in_col = set()
+    max_in_row = set()  # O(1) space
+    min_in_col = set()  # O(1) space
 
     # For each row in the matrix, find the maximum element in that row and add it to max_in_row set
-    for row in matrix:
-        max_in_row.add(max(row))
+    for row in matrix:  # O(m*n) time
+        max_in_row.add(max(row))    # O(n) time
 
     # For each column in the matrix, find the minimum element in that column and add it to min_in_col set
-    for col in range(len(matrix[0])):
+    for col in range(len(matrix[0])):   # O(n) time
         # To find the minimum element in a column, we can iterate through all rows and take the minimum value
         # of the element in that column using a generator expression
-        min_in_col.add(min(row[col] for row in matrix))
+        min_in_col.add(min(row[col] for row in matrix)) # O(m) time
 
     # Return the intersection of the two sets, which contains only the elements that are in both sets
-    return max_in_row.intersection(min_in_col)
+    return max_in_row.intersection(min_in_col)  # O(min(m,n)) time, O(min(m,n)) space
 
 # Tests
 # Test case 1
@@ -27,10 +27,8 @@ assert max_row_min_col(matrix) == {50}
 
 # Test case 3
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-result = max_row_min_col(matrix)
-assert result == {3, 6}, f"Failed: {result}"
+assert max_row_min_col(matrix) == {3}
 
 # Test case 4
 matrix = [[5, 16, 20], [9, 11, 18], [15, 16, 20]]
-result = max_row_min_col(matrix)
-assert result == {16}, f"Failed: {result}"
+assert max_row_min_col(matrix) == {16}
